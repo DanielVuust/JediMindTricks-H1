a90f0d55cf1c3853bf5d732cc61de56f27f0d7db
DanielVuust <Daniel.vuust@gmail.com>
2021-02-23 09:03:29 +0100

    Add project files.

---
+++ JediMindTricks/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JediMindTricks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> Dictionary = new Dictionary<string, int>()
+            {
+                {"Han", 25}
+            };
+            Dictionary.Add("Daniel", 17);
+            Dictionary["Peter"] = 21;
+
+            foreach( KeyValuePair<string, int> pair in Dictionary)
+            {
+                Console.WriteLine($"{pair.Key} er {pair.Value} år gammel.");
+
+            }
+
+            Dictionary.Remove("Han");
+
+            Console.WriteLine();
+            foreach (var pair in Dictionary)
+            {
+                Console.WriteLine($"{pair.Key} er {pair.Value} år gammel.");
+
+            }
+        }
+    }
+}
